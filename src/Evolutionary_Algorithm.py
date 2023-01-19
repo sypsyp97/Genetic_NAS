5878cede0d33590315faeaf3a1aa957bc34eb74b
Yipeng Sun <sypsyp97@gmail.com>
2023-01-19 05:21:38 +0100

    update the crossover function

--- src/Evolutionary_Algorithm.py
+++ src/Evolutionary_Algorithm.py
@@ -6,42 +6,42 @@ from src.Train_Model import train_model
 
 
 def create_first_population(population=10):
-
     first_population_array = np.random.randint(0, 2, (population, 9, 18))
 
     return first_population_array
 
 
-def select_best_2_model(train_ds, val_ds, test_ds,
-                        population_array=np.random.randint(0, 2, (10, 9, 18)),
-                        epochs=20, num_classes=2):
-    accuracies = []
+def select_best_2_model(train_ds,
+                        val_ds,
+                        test_ds,
+                        population_array,
+                        epochs=20,
+                        num_classes=2):
+    fitness_list = []
     # tflite_accuracies = []
     for i in range(population_array.shape[0]):
         model = create_model(population_array[i], num_classes=num_classes)
         model_summary(model)
         trained_model, history = train_model(train_ds, val_ds, model=model, epochs=epochs)
         acc = model_evaluation(trained_model, test_ds)
-        accuracies.append(acc)
+        fitness_list.append(acc)
 
     # TODO: Use custom fitness function to select the individual.
 
-    best_models_indices = sorted(range(len(accuracies)), key=lambda i: accuracies[i], reverse=True)[:2]
+    best_models_indices = sorted(range(len(fitness_list)), key=lambda i: fitness_list[i], reverse=True)[:2]
     best_models_array = [population_array[i] for i in best_models_indices]
 
     return best_models_array[0], best_models_array[1]
 
 
 def crossover(parent_1_array, parent_2_array):
-    child_array = np.random.randint(0, 2, (9, 18))
-    for i in range(9):
-        for j in range(18):
-            child_array[i, j] = np.random.choice([parent_1_array[i, j], parent_2_array[i, j]])
-
+    mask = np.random.permutation(np.concatenate((np.zeros(9*18//2),
+                                                 np.ones(9*18//2)))).reshape(9, 18).astype(np.bool_)
+    child_array = np.where(mask, parent_1_array, parent_2_array)
     return child_array
 
 
-def mutate(model_array=np.random.randint(0, 2, (9, 18)), mutate_prob=0.02):
+def mutate(model_array, mutate_prob=0.02):
     prob = np.random.uniform(size=(9, 18))
     mutated_array = np.where(prob < mutate_prob, np.logical_not(model_array), model_array)
 
