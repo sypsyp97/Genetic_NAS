752ea97c9563dfb63e4b8a2fd804cf2434794f8a
Yipeng Sun <sypsyp97@gmail.com>
2023-01-19 05:47:12 +0100

    update

--- src/TFLITE_Converter.py
+++ src/TFLITE_Converter.py
@@ -1,23 +1,18 @@
 import tensorflow as tf
 
 
-def convert_to_tflite(x_test, model):
+def convert_to_tflite(x_test, keras_model):
     def representative_data_gen():
         for data in tf.data.Dataset.from_tensor_slices(x_test).batch(1).take(100):
             yield [(tf.dtypes.cast(data, tf.float32))]
 
-    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    converter = tf.lite.TFLiteConverter.from_keras_model(keras_model)
 
     # This enables quantization
     converter.optimizations = [tf.lite.Optimize.DEFAULT]
 
     # This sets the representative dataset for quantization
     converter.representative_dataset = representative_data_gen
-
-    # This ensures that if any ops can't be quantized, the converter throws an error
-
-    # converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8,
-    #                                        tf.lite.OpsSet.SELECT_TF_OPS]
 
     # For full integer quantization, though supported types defaults to int8 only, we explicitly declare it for clarity.
     converter.target_spec.supported_types = [tf.int8]
@@ -31,7 +26,7 @@ def convert_to_tflite(x_test, model):
     converter.experimental_new_quantizer = True
 
     tflite_model = converter.convert()
-    tf.lite.experimental.Analyzer.analyze(model_content=tflite_model, gpu_compatibility=True)
+    # tf.lite.experimental.Analyzer.analyze(model_content=tflite_model, gpu_compatibility=True)
 
     # with open('test.tflite', 'wb') as f:
     #     f.write(tflite_model)
