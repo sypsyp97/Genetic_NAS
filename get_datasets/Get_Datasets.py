6a9347234a565612fffd5d9fdd03acca0bce6545
liuaijia <ge95hul@mytum.de>
2023-01-25 12:08:52 +0100

    Optimize the datagetter

--- get_datasets/Get_Datasets.py
+++ get_datasets/Get_Datasets.py
@@ -1,36 +1,42 @@
 from os import listdir
 from os.path import join
 
-import cv2
-import numpy as np
-import tensorflow as tf
 from imgaug import augmenters as iaa
 from sklearn.model_selection import train_test_split
 from tqdm.notebook import tqdm
+
+from concurrent.futures import ProcessPoolExecutor
+from functools import partial
+import numpy as np
+import cv2
+import tensorflow as tf
+
+
+def process_image(img_path, img_size=128, class_num=2):
+    try:
+        img_arr = cv2.imread(img_path)
+        resized_arr = cv2.resize(img_arr, (img_size, img_size), interpolation=cv2.INTER_AREA)
+        return resized_arr, class_num
+    except Exception as e:
+        print(e)
 
 
 def get_data(data_dir, img_size=128, labels=None):
     if labels is None:
         labels = ['PNEUMONIA', 'NORMAL']
     data = []
-    for label in labels:
-        path = join(data_dir, label)
-        class_num = labels.index(label)
-        for img in tqdm(listdir(path)):
-            try:
-                img_arr = cv2.imread(join(path, img))
-                resized_arr = tf.image.resize(img_arr, (img_size, img_size))  # Reshaping images to preferred size
-                data.append((resized_arr, class_num))
-            except Exception as e:
-                print(e)
+    with ProcessPoolExecutor() as executor:
+        for label in labels:
+            path = join(data_dir, label)
+            class_num = labels.index(label)
+            img_paths = [join(path, img) for img in listdir(path)]
+            data += executor.map(partial(process_image, img_size=img_size, class_num=class_num), img_paths)
     return data
 
 
 def get_data_array(train_dir, test_dir, img_size=128, num_classes=2):
-    train_data = get_data(train_dir)
-    test_data = get_data(test_dir)
-    x_train, y_train = zip(*train_data)
-    x_test, y_test = zip(*test_data)
+    x_train, y_train = zip(*get_data(train_dir, img_size))
+    x_test, y_test = zip(*get_data(test_dir, img_size))
 
     x_train = np.array(x_train)
     x_test = np.array(x_test)
@@ -47,6 +53,44 @@ def get_data_array(train_dir, test_dir, img_size=128, num_classes=2):
     return x_train, y_train, x_val, y_val, x_test, y_test
 
 
+# def get_data(data_dir, img_size=128, labels=None):
+#     if labels is None:
+#         labels = ['PNEUMONIA', 'NORMAL']
+#     data = []
+#     for label in labels:
+#         path = join(data_dir, label)
+#         class_num = labels.index(label)
+#         for img in tqdm(listdir(path)):
+#             try:
+#                 img_arr = cv2.imread(join(path, img))
+#                 resized_arr = tf.image.resize(img_arr, (img_size, img_size))  # Reshaping images to preferred size
+#                 data.append((resized_arr, class_num))
+#             except Exception as e:
+#                 print(e)
+#     return data
+#
+#
+# def get_data_array(train_dir, test_dir, img_size=128, num_classes=2):
+#     train_data = get_data(train_dir)
+#     test_data = get_data(test_dir)
+#     x_train, y_train = zip(*train_data)
+#     x_test, y_test = zip(*test_data)
+#
+#     x_train = np.array(x_train)
+#     x_test = np.array(x_test)
+#
+#     x_train = x_train.reshape(-1, img_size, img_size, 3)
+#     x_test = x_test.reshape(-1, img_size, img_size, 3)
+#
+#     x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.1)
+#
+#     y_train = tf.one_hot(y_train, num_classes)
+#     y_val = tf.one_hot(y_val, num_classes)
+#     y_test = tf.one_hot(y_test, num_classes)
+#
+#     return x_train, y_train, x_val, y_val, x_test, y_test
+
+
 def augment(images, seed=0):
     rand_aug = iaa.RandAugment(n=2, m=(0, 7), seed=seed)
     images = tf.cast(images, tf.uint8)
@@ -55,7 +99,6 @@ def augment(images, seed=0):
 
 def get_datasets(x_train, y_train, x_val, y_val, x_test, y_test,
                  auto=tf.data.AUTOTUNE, batch_size=16):
-
     train_ds_rand = (
         tf.data.Dataset.from_tensor_slices((x_train, y_train))
         .shuffle(batch_size * 100)
