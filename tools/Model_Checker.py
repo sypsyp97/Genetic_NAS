99615dbc46cfd80fd48a75a22e2f7aaf5a5c2bf9
Yipeng Sun <sypsyp97@gmail.com>
2023-01-25 15:28:40 +0100

    Add Model_Checker.py

---
+++ tools/Model_Checker.py
@@ -0,0 +1,14 @@
+from keras import Model
+
+
+def check_model(model):
+    for layer in model.layers:
+        # Check if the layer is a MultiHeadAttention layer
+        if 'multi_head_attention' in str(layer):
+            # Get the output shape of the layer
+            output_shape = layer.output.shape
+            # Get the size of the second dimension
+            size = output_shape[1]
+            if size > 1024:
+                return False
+    return True
