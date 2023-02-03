8741eedb604d3739b4f376070d2f4d8f89a05204
liuaijia <ge95hul@mytum.de>
2023-02-03 17:53:54 +0100

    Update

--- tf_flower_test.py
+++ tf_flower_test.py
@@ -77,7 +77,7 @@ if __name__ == '__main__':
     population_array, max_fitness_history, average_fitness_history, a, b = start_evolution(train_ds=train_dataset,
                                                                                            val_ds=val_dataset,
                                                                                            test_ds=val_dataset,
-                                                                                           generations=15,
-                                                                                           population=15,
+                                                                                           generations=16,
+                                                                                           population=16,
                                                                                            num_classes=5,
-                                                                                           epochs=30)
+                                                                                           epochs=24)
