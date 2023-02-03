dce6990ecf49fcc1bc624eab263b0b39c0b43f37
liuaijia <ge95hul@mytum.de>
2023-02-03 12:35:52 +0100

    Update

--- src/Create_Model.py
+++ src/Create_Model.py
@@ -27,17 +27,19 @@ it adds a dense layer with num_classes number of units and returns the model.'''
 
 def create_model(model_array, num_classes=5, input_shape=(256, 256, 3)):
     with strategy.scope():
-        inputs = layers.Input(shape=input_shape)
-        x = layers.Rescaling(scale=1.0 / 255)(inputs)
-        x = conv_block(x, kernel_size=2, filters=16, strides=2)
+    inputs = layers.Input(shape=input_shape)
+    x = layers.Rescaling(scale=1.0 / 255)(inputs)
+    x = conv_block(x, kernel_size=2, filters=16, strides=2)
 
-        for i in range(9):
-            x = decoded_block(x, model_array[i])
+    for i in range(9):
+        x = decoded_block(x, model_array[i])
 
-        x = conv_block(x, filters=320, kernel_size=1, strides=1)
-        x = layers.GlobalAvgPool2D()(x)
+    x = conv_block(x, filters=320, kernel_size=1, strides=1)
+    x = layers.GlobalAvgPool2D()(x)
 
-        outputs = layers.Dense(num_classes, activation="softmax")(x)
+    outputs = layers.Dense(num_classes, activation="softmax")(x)
+
+    with strategy.scope():
         model = keras.Model(inputs, outputs)
 
     return model
