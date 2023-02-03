eaae244fbb106c2d3ae052f6a8ee216aeba82d48
liuaijia <ge95hul@mytum.de>
2023-02-03 12:36:03 +0100

    Update

--- src/Create_Model.py
+++ src/Create_Model.py
@@ -26,7 +26,7 @@ it adds a dense layer with num_classes number of units and returns the model.'''
 
 
 def create_model(model_array, num_classes=5, input_shape=(256, 256, 3)):
-    with strategy.scope():
+
     inputs = layers.Input(shape=input_shape)
     x = layers.Rescaling(scale=1.0 / 255)(inputs)
     x = conv_block(x, kernel_size=2, filters=16, strides=2)
