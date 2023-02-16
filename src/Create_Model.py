c9c9815c2c2ab4f113e06830ea1e36ccff9ac7f0
Yipeng Sun <sypsyp97@gmail.com>
2023-02-16 23:10:22 +0100

    Update
    Signed-off-by: liuaijia <ge95hul@mytum.de>

--- src/Create_Model.py
+++ src/Create_Model.py
@@ -47,10 +47,40 @@ def model_summary(model):
     model.summary()
     print('Number of trainable weights = {}'.format(len(model.trainable_weights)))
 
+# def train_model(train_ds, val_ds,
+#                 model, epochs=20,
+#                 checkpoint_filepath="checkpoints/checkpoint"):
+#     checkpoint_callback = keras.callbacks.ModelCheckpoint(checkpoint_filepath,
+#                                                           monitor="val_accuracy",
+#                                                           save_best_only=True,
+#                                                           save_weights_only=True)
+#
+#     loss_fn = keras.losses.CategoricalCrossentropy(label_smoothing=0.1)
+#
+#     opt = tfa.optimizers.LazyAdam(learning_rate=0.002)
+#     opt = tfa.optimizers.MovingAverage(opt)
+#     opt = tfa.optimizers.Lookahead(opt)
+#
+#     model.compile(optimizer=opt,
+#                   loss=loss_fn,
+#                   metrics=['accuracy'])
+#
+#     try:
+#         history = model.fit(train_ds,
+#                             epochs=epochs,
+#                             validation_data=val_ds,
+#                             callbacks=[checkpoint_callback])
+#
+#         model.load_weights(checkpoint_filepath)
+#     except Exception as e:
+#         history = None
+#         print(e)
+#     return model, history
 
 def train_model(train_ds, val_ds,
                 model, epochs=20,
-                checkpoint_filepath="checkpoints/checkpoint"):
+                checkpoint_filepath="checkpoints/checkpoint",
+                early_stopping_patience=5):
     checkpoint_callback = keras.callbacks.ModelCheckpoint(checkpoint_filepath,
                                                           monitor="val_accuracy",
                                                           save_best_only=True,
@@ -66,14 +96,19 @@ def train_model(train_ds, val_ds,
                   loss=loss_fn,
                   metrics=['accuracy'])
 
+    early_stopping_callback = keras.callbacks.EarlyStopping(monitor='val_loss',
+                                                            patience=early_stopping_patience,
+                                                            restore_best_weights=True)
+
     try:
         history = model.fit(train_ds,
                             epochs=epochs,
                             validation_data=val_ds,
-                            callbacks=[checkpoint_callback])
+                            callbacks=[checkpoint_callback, early_stopping_callback])
 
         model.load_weights(checkpoint_filepath)
     except Exception as e:
         history = None
         print(e)
     return model, history
+
