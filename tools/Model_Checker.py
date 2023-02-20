3b2fd499ca304e96746b966c4ac100eed8c1c7fc
liuaijia <ge95hul@mytum.de>
2023-02-20 02:13:52 +0100

    Update
    Signed-off-by: liuaijia <ge95hul@mytum.de>

--- tools/Model_Checker.py
+++ tools/Model_Checker.py
@@ -6,18 +6,37 @@ dimension. If it's greater than 1024, it returns True. Otherwise, it continues t
 find any such layer, it returns False."""
 
 
-def check_model(model):
-    for layer in model.layers:
-        # Check if the layer is a MultiHeadAttention layer
-        if 'multi_head_attention' in str(layer):
-            # Get the output shape of the layer
-            output_shape = layer.output.shape
-            # Get the size of the second dimension
-            size = output_shape[1]
-            if size > 1024:
-                return True
-            else:
-                return False
-        else:
-            return True
+# def check_model(model):
+#     for layer in model.layers:
+#         # Check if the layer is a MultiHeadAttention layer
+#         if 'multi_head_attention' in str(layer):
+#             # Get the output shape of the layer
+#             output_shape = layer.output.shape
+#             # Get the size of the second dimension
+#             size = output_shape[1]
+#             if size > 1024:
+#                 return True
+#     return False
 
+
+def check_model(model):
+    # Check if model contains a layer with the name 'multi_head_attention'
+    contains_multi_head_attention = False
+    for layer in model.layers:
+        if 'multi_head_attention' in str(layer):
+            contains_multi_head_attention = True
+            break
+
+    # If model contains a layer with the name 'multi_head_attention', check if output size > 1024
+    if contains_multi_head_attention:
+        for layer in model.layers:
+            if 'multi_head_attention' in str(layer):
+                output_shape = layer.output.shape
+                size = output_shape[1]
+                if size > 1024:
+                    return True
+        return False
+
+    # If model does not contain a layer with the name 'multi_head_attention', return False
+    else:
+        return False
