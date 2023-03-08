25a2cd7e753eec5dc99495db3281967d2a4cc2ac
Yipeng <sypsyp97@gmail.com>
2023-03-08 16:13:37 +0100

    1

--- tools/Compile_Edge_TPU.py
+++ tools/Compile_Edge_TPU.py
@@ -1,13 +1,13 @@
 import os
 
 
-def compile_edgetpu(tflite_model_name):
-    if not os.path.exists(tflite_model_name):
-        print(f"{tflite_model_name} not found")
+def compile_edgetpu(path):
+    if not os.path.exists(path):
+        print(f"{path} not found")
         return
 
-    edgetpu_model_name = tflite_model_name.replace('.tflite', '_edgetpu.tflite')
-    os.system('edgetpu_compiler -sa {}'.format(tflite_model_name))
+    edgetpu_model_name = path.replace('.tflite', '_edgetpu.tflite')
+    os.system('edgetpu_compiler -sa {}'.format(path))
 
     return edgetpu_model_name
 
