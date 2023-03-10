7164ae683a0e808d4670a9a3732cf02ce55c29bc
Yipeng <sypsyp97@gmail.com>
2023-03-10 12:10:38 +0100

    1

--- tools/Create_Model.py
+++ tools/Create_Model.py
@@ -5,20 +5,8 @@ import tensorflow as tf
 
 from src.Decode_Block import decoded_block
 from src.Gene_Pool import conv_block
-
-
-'''This function takes in 3 inputs, model_array, num_classes and input_shape. The function creates a keras model by defining the layers in it.
-
-It starts by creating an input layer with the shape specified by the input_shape variable. Then it applies a 
-rescaling layer with a scale factor of 1/255 to the input. It then applies a convolutional block with a kernel size 
-of 2, 16 filters and a stride of 2 to the input.
-
-It then enters a for loop that iterates 9 times. On each iteration, it applies a decoded_block function to the 
-current output, passing in the current element of the model_array.
-
-After the for loop, it applies another convolutional block with 320 filters, a kernel size of 1 and a stride of 1 to 
-the output. It then applies a global average pooling layer and a dropout layer with a rate of 0.5. Finally, 
-it adds a dense layer with num_classes number of units and returns the model.'''
+"""
+"""
 
 
 def create_model(model_array, num_classes=5, input_shape=(256, 256, 3)):
