310f52ee67bd587958bc46713d0b65ba656404f5
Yipeng <sypsyp97@gmail.com>
2023-03-13 14:34:05 +0100

    1

--- tools/TFLITE_Converter.py
+++ tools/TFLITE_Converter.py
@@ -1,24 +1,3 @@
-'''The convert_to_tflite function takes in two arguments: x_test and keras_model.
-x_test is a test dataset that is used to generate representative data for the quantization process.
-keras_model is the model that you want to convert to TFLite format.
-
-The function first defines a representative_data_gen() function that yields a batch of data from x_test, 
-which is used to generate representative data for the quantization process.
-
-It then creates a TFLiteConverter object from the keras_model and sets the optimizations to the default value. The 
-representative dataset is set as the output of the representative_data_gen() function.
-
-It sets the target_spec.supported_types to int8, which enables full integer quantization.
-
-It also set the input and output tensors to uint8, which is necessary for full integer quantization.
-
-The allow_custom_ops, experimental_new_converter, and experimental_new_quantizer are set to true to enable some new 
-features.
-
-Then it calls the convert() method on the converter object which returns the TFLite model in bytes.
-
-The tflite_model is returned by the function, which can be written to a file or used in other ways as needed.'''
-
 import tensorflow as tf
 from get_datasets.Data_for_TFLITE import x_test
 
@@ -26,6 +5,23 @@ from get_datasets.Data_for_TFLITE import x_test
 def representative_data_gen():
     for data in tf.data.Dataset.from_tensor_slices(x_test).batch(1).take(100):
         yield [(tf.dtypes.cast(data, tf.float32))]
+
+
+"""Function Signature:
+def convert_to_tflite(keras_model: keras.Model, generation: int, i: int, time: str) -> Tuple[bytes, str]
+
+Parameters:
+keras_model: A Keras model object representing the TensorFlow model to be converted to TensorFlow Lite.
+generation: An integer representing the generation number of the model.
+i: An integer representing the index number of the model within the generation.
+time: A string representing the timestamp of the conversion.
+
+Returns:
+A tuple containing the converted TensorFlow Lite model as a byte string and the path to the saved model file. 
+Description: The "convert_to_tflite" function converts the input Keras model to a TensorFlow Lite model with 
+quantization enabled. The function returns a tuple containing the converted model as a byte string and the path to 
+the saved model file. The saved model file name includes the generation number, index number, and timestamp.
+"""
 
 
 def convert_to_tflite(keras_model, generation, i, time):
