7c2de5dca4eb80d5c87b744fc8a99a8226efc6b8
Yipeng Sun <sypsyp97@gmail.com>
2023-03-26 21:38:39 +0200

    update

--- src/Fitness_Function.py
+++ src/Fitness_Function.py
@@ -4,6 +4,6 @@ from math import pi
 
 
 def calculate_fitness(accuracy, inference_time=1, memory_footprint_tflite=1):
-    fitness = (1 - np.arctan(inference_time/500) / (pi / 2)) * accuracy
+    fitness = (1 - np.arctan(inference_time / 500) / (pi / 2)) * accuracy
 
     return fitness
