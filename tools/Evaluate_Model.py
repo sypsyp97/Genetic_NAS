7a45333f937e2fbf623eb39cd8001e490d4c5796
Yipeng Sun <sypsyp97@gmail.com>
2023-03-27 21:11:06 +0200

    update

--- tools/Evaluate_Model.py
+++ tools/Evaluate_Model.py
@@ -28,47 +28,54 @@ The function returns the accuracy of the model.
 
 
 def evaluate_tflite_model(tflite_model, tfl_int8=True):
-    interpreter = tflite.Interpreter(model_path=tflite_model, experimental_delegates=[tflite.load_delegate('libedgetpu.so.1')])
-    interpreter.allocate_tensors()
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
-    input_index = input_details[0]["index"]
-    output_index = output_details[0]["index"]
-    scale_in, zero_point_in = input_details[0]['quantization']
-    scale_out, zero_point_out = output_details[0]['quantization']
+    try:
+        interpreter = tflite.Interpreter(model_path=tflite_model, experimental_delegates=[tflite.load_delegate('libedgetpu.so.1')])
+        interpreter.allocate_tensors()
+        input_details = interpreter.get_input_details()
+        output_details = interpreter.get_output_details()
+        input_index = input_details[0]["index"]
+        output_index = output_details[0]["index"]
+        scale_in, zero_point_in = input_details[0]['quantization']
+        scale_out, zero_point_out = output_details[0]['quantization']
 
-    prediction_labels = []
-    test_labels = []
-    inference_speeds = []
+        prediction_labels = []
+        test_labels = []
+        inference_speeds = []
 
-    for i in range(x_test.shape[0]):
-        if tfl_int8:
-            test_image = x_test[i] / scale_in + zero_point_in
-            test_image = np.expand_dims(test_image, axis=0).astype(np.uint8)
-        else:
-            test_image = np.expand_dims(x_test[i], axis=0).astype(np.float32)
+        for i in range(x_test.shape[0]):
+            if tfl_int8:
+                test_image = x_test[i] / scale_in + zero_point_in
+                test_image = np.expand_dims(test_image, axis=0).astype(np.uint8)
+            else:
+                test_image = np.expand_dims(x_test[i], axis=0).astype(np.float32)
 
-        interpreter.set_tensor(input_index, test_image)
-        start_time = time.monotonic()
-        interpreter.invoke()
-        tpu_inference_time = (time.monotonic() - start_time) * 1000
-        inference_speeds.append(tpu_inference_time)
+            interpreter.set_tensor(input_index, test_image)
+            start_time = time.monotonic()
+            interpreter.invoke()
+            tpu_inference_time = (time.monotonic() - start_time) * 1000
+            inference_speeds.append(tpu_inference_time)
 
-        output = interpreter.get_tensor(output_index)
-        if tfl_int8:
-            output = output.astype(np.float32)
-            output = (output - zero_point_out) * scale_out
-        digit = np.argmax(output[0])
-        prediction_labels.append(digit)
-        test_labels.append(np.argmax(y_test[i], ))
+            output = interpreter.get_tensor(output_index)
+            if tfl_int8:
+                output = output.astype(np.float32)
+                output = (output - zero_point_out) * scale_out
+            digit = np.argmax(output[0])
+            prediction_labels.append(digit)
+            test_labels.append(np.argmax(y_test[i], ))
 
-    prediction_labels = np.array(prediction_labels)
-    test_labels = np.array(test_labels)
-    tflite_accuracy = tf.keras.metrics.Accuracy()
-    tflite_accuracy(prediction_labels, test_labels)
+        prediction_labels = np.array(prediction_labels)
+        test_labels = np.array(test_labels)
+        tflite_accuracy = tf.keras.metrics.Accuracy()
+        tflite_accuracy(prediction_labels, test_labels)
 
-    del interpreter
-    del input_details
-    del output_details
+        del interpreter
+        del input_details
+        del output_details
+
+    except Exception as e:
+        tflite_accuracy = tf.keras.metrics.Accuracy()
+        inference_speeds = 9999
+
+        print(e)
 
     return float(tflite_accuracy.result()), np.average(inference_speeds)
