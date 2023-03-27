af93e60c411f9d913659b36a67d0e36d908d95b6
Yipeng Sun <sypsyp97@gmail.com>
2023-03-27 21:52:54 +0200

    update

--- tools/Model_Checker.py
+++ tools/Model_Checker.py
@@ -12,26 +12,6 @@ contains a layer of type "multi_head_attention" and if the output shape of the l
 function returns True if the model does not contain a multi-head attention layer or if the output shape of the layer
 is less than or equal to 1024, otherwise it returns False."""
 
-#
-# def model_has_problem(model):
-#     contains_multi_head_attention = False
-#     for layer in model.layers:
-#         if 'multi_head_attention' in str(layer):
-#             contains_multi_head_attention = True
-#             break
-#
-#     if contains_multi_head_attention:
-#         for layer in model.layers:
-#             if 'multi_head_attention' in str(layer):
-#                 output_shape = layer.output.shape
-#                 size = output_shape[1]
-#                 if size > 1024:
-#                     return True
-#         return False
-#
-#     else:
-#         return True
-
 
 from tools.TFLITE_Converter import convert_to_tflite
 from tools.Compile_Edge_TPU import compile_edgetpu
